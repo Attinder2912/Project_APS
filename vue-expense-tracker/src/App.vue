af190a7c3c447fbef0bacf870ef6cff27b7ca7cd
Attinder <attinderpal.singh@gmail.com>
2024-03-09 16:57:36 +1100

    Added and Deletion of new added item in local Stoage

--- vue-expense-tracker/src/App.vue
+++ vue-expense-tracker/src/App.vue
@@ -3,7 +3,9 @@
   <div class="container">
     <Balance :total="total"></Balance>
     <IncomeExpense :income="+income" :expenses="+expenses"></IncomeExpense>
-    <TransitionList :transactions="transactions"></TransitionList>
+      <TransitionList :transactions="transactions" 
+      @transactionDeleted="handleTransactionDeleted">
+      </TransitionList>
     <AddTransaction @transactionSubmitted="handleTrasnactionSubmitted"></AddTransaction>
   </div>
 </template>
@@ -14,15 +16,28 @@
   import IncomeExpense from './components/IncomeExpense.vue';
   import TransitionList from './components/TransactionList.vue'; // Corrected import path
   import AddTransaction from './components/AddTransaction.vue'; // Corrected import path
-  import { ref ,computed} from 'vue';
+  import { ref ,computed, onMounted} from 'vue';
 
+  import { useToast } from 'vue-toastification';
+
+  const toast=useToast();
+//local storage rather than giving data directly like below
+
+  onMounted(()=>{
+    const savedTransaction=JSON.parse(localStorage.getItem('transactions'));
+
+    if(savedTransaction){
+      transactions.value=savedTransaction;
+    }
+  })
 // ref to make reactive things
-  const transactions = ref([
-    { id: 1, text: 'Flower', amount: -19.99 },
-    { id: 2, text: 'Salary', amount: 299.97 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 },
-  ]);
+  // const transactions = ref([
+  //   { id: 1, text: 'Flower', amount: -19.99 },
+  //   { id: 2, text: 'Salary', amount: 299.97 },
+  //   { id: 3, text: 'Book', amount: -10 },
+  //   { id: 4, text: 'Camera', amount: 150 },
+  // ]);
+  const transactions = ref([ ]);
 //get total
   const total=computed(()=>{
     return transactions.value.reduce((acc,transactions)=>{
@@ -54,13 +69,28 @@ const handleTrasnactionSubmitted = (transactionData)=>{
  transactions.value.push({
   id:generateUniqueId(),
   text:transactionData.text,
-  amoutn:transactionData.amount
+  amount:transactionData.amount
  });
+ saveTransactionToLocalStorage();
  //console.log(generateUniqueId());
+ toast.success('Transaction Added');
+
 };
 //generate Unique id
 const generateUniqueId=()=>{
   return Math.floor(Math.random()*1000000);
+};
+//Delete Transaction
+const handleTransactionDeleted= (id)=>{
+  //console.log(id);
+  transactions.value=transactions.value.filter(
+    (transactions)=>transactions.id !==id
+  );
+  toast.success('Transaction Deleted')
+};
+//save to localStorage
+const saveTransactionToLocalStorage = ()=>{
+  localStorage.setItem('transactions',JSON.stringify(transactions.value));
 }
   </script>
 
--- vue-expense-tracker/src/components/TransactionList.vue
+++ vue-expense-tracker/src/components/TransactionList.vue
@@ -3,20 +3,25 @@
     <ul id="list" class="list">
       <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
         {{ transaction.text }}<span>{{ transaction.amount }}</span>
-        <button class="delete-btn">x</button>
+        <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
       </li>
     </ul>
   </template>
   
   <script setup>
-  import { defineProps } from 'vue';
+  import { defineProps,defineEmits } from 'vue';
   
+  const emit=defineEmits(['transactionDeleted'])
+
   const props = defineProps({
     transactions: {
       type: Array,
       required: true,
     },
   });
+  const deleteTransaction = (id)=>{
+    emit('transactionDeleted',id);
+  }
   </script>
   
 <!--taking this to app.vue to call as a compostion API-->
